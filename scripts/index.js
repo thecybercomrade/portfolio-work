c1efa3806a800c5b419f2ab1d38b71acf0ac703a
thecybercomrade <65641159+thecybercomrade@users.noreply.github.com>
2021-03-30 22:31:28 +0530

    menu and slide nav changes commit

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+const sideMenu = document.querySelector(".slider-nav");
+
+showMenu = () => {
+    sideMenu.style.display = "block";
+}
+
+hideMenu = () => {
+    sideMenu.style.display = "none";
+}
