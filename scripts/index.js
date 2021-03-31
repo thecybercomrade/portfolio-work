7d7dc2728563ef1094693e4113a5c0ee079751cc
thecybercomrade <65641159+thecybercomrade@users.noreply.github.com>
2021-03-31 07:45:17 +0530

    menu animation addition changes commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 const sideMenu = document.querySelector(".slider-nav");
 
+const mediaQuery = window.matchMedia("(max-width: 796px)");
+
 showMenu = () => {
     sideMenu.style.display = "block";
 }
@@ -7,3 +9,12 @@ showMenu = () => {
 hideMenu = () => {
     sideMenu.style.display = "none";
 }
+
+handleTabletChange = (e) => {
+if (!e.matches) {
+    sideMenu.style.display = "none";
+  }
+};
+
+mediaQuery.addListener(handleTabletChange);
+handleTabletChange(mediaQuery);
